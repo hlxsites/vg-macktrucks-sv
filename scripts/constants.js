eb76c900ce2610be2a3cf8ce3997533c52faf9fb
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2024-04-05 10:21:32 +0200

    676 step 1: add OT ID & add constants.js

---
+++ scripts/constants.js
@@ -0,0 +1,15 @@
+// check if an active campaign is running or OneTrust needs scan the active scripts
+export const COOKIE_CHECK = true;
+
+// ONE TRUST COOKIE CONSENT
+export const DATA_DOMAIN_SCRIPT = 'f32a5898-5d20-4ec9-8ce7-20702edd7c2f';
+
+// GOOGLE TAG MANAGER ID
+export const GTM_ID = 'GTM-5DKKVHFL';
+
+// COOKIE Constants
+export const COOKIE_VALUES = {
+  performance: 'C0002',
+  targeting: 'C0004',
+  social: 'C0005',
+};
